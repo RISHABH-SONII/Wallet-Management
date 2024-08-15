267bdcfa9b258b5b03bbf6e6f53b9472e9a37da3
RISHABH-SONII <rishabhsoni50414@gmail.com>
2024-08-15 16:49:13 +0530

    Adding New File Under Pages Folder To Show The Users Profile

---
+++ ui/src/Pages/UserProfile.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from '../Common/Header'
+
+export default function UserProfile() {
+  return (
+    <div>
+            <Header/>
+            <h1>UserProfile Page</h1>
+        </div>
+  )
+}
