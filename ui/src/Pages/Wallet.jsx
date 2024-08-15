03a48defbfb96b4b8ea271dc2baeb31e9e15b15b
RISHABH-SONII <rishabhsoni50414@gmail.com>
2024-08-15 16:53:00 +0530

    Adding New File Under Pages Folder To Show The Users Wallet Details

---
+++ ui/src/Pages/Wallet.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from '../Common/Header'
+
+export default function Wallet() {
+  return (
+    <div>
+    <Header/>
+    <h1>Wallet Page</h1>
+</div>
+  )
+}
