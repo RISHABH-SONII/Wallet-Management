1842ecf7041c1eb3e14620be3ca4719724039d91
RISHABH-SONII <rishabhsoni50414@gmail.com>
2024-08-15 16:59:23 +0530

    Adding New File To Show The Notifications

---
+++ ui/src/Pages/Notifications.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from '../Common/Header'
+
+export default function Notifications() {
+  return (
+    <div>
+            <Header/>
+            <h1>Notifications Page</h1>
+        </div>
+  )
+}
