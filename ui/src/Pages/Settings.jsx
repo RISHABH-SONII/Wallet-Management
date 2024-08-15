08828c2ca59a4471408da9b6f264e1c20b8677d9
RISHABH-SONII <rishabhsoni50414@gmail.com>
2024-08-15 17:01:23 +0530

    Adding New File For Settings Page

---
+++ ui/src/Pages/Settings.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from '../Common/Header'
+
+export default function Settings() {
+  return (
+    <div>
+    <Header/>
+    <h1>Settings Page</h1>
+</div>
+  )
+}
