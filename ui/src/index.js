88aaacc725295ae0ec0e8cba97b365df000f2935
RISHABH-SONII <rishabhsoni50414@gmail.com>
2024-08-15 16:39:08 +0530

    Setting Routes For Different Pages And Remove The App files component

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,16 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.css"
 import "bootstrap/dist/js/bootstrap.bundle"
+import HomePage from './Pages/HomePage';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Notifications from './Pages/Notifications';
+import UserProfile from './Pages/UserProfile';
+import Wallet from './Pages/Wallet';
+import Settings from './Pages/Settings';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+let allroutes=createBrowserRouter(
+  [
+    {
+      path:"/",
+      element:<HomePage/>
+    },
+    {
+      path:"/userProfile",
+      element:<UserProfile/>
+    },
+    {
+      path:"/wallet",
+      element:<Wallet/>
+    },
+    {
+      path:"/notification",
+      element:<Notifications/>
+    },
+    {
+      path:"/settings",
+      element:<Settings/>
+    }
+  ]
+)
 root.render(
 
   <React.StrictMode>
-    <App />
+    <RouterProvider router={allroutes}/>
   </React.StrictMode>
 );
 
