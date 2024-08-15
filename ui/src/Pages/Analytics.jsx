99875f53d143ca0d963ffe01868fef11c222bda2
RISHABH-SONII <rishabhsoni50414@gmail.com>
2024-08-15 16:57:31 +0530

    Adding New File To Show The Analytics Of The Users Transections In The Form Chart-Bar Or Graph-Bar

---
+++ ui/src/Pages/Analytics.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from '../Common/Header'
+
+export default function Analytics() {
+  return (
+    <div>
+            <Header/>
+            <h1>Analytics Page</h1>
+        </div>
+  )
+}
