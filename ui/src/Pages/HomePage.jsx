2f79d3014e1394ce2b16482084bd4b21a9ce86bb
RISHABH-SONII <rishabhsoni50414@gmail.com>
2024-08-15 16:41:58 +0530

    Making HomePage Design For To Show The Dashboard To User

--- ui/src/Pages/HomePage.jsx
+++ ui/src/Pages/HomePage.jsx
@@ -1,117 +1,118 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBarChart, faBell, faCog, faHome, faPersonBooth, faPersonRifle, faSearch, faUser, faWallet } from '@fortawesome/free-solid-svg-icons';
-import { Container, Row, Col, Form, InputGroup, Button } from 'react-bootstrap';
+import React from 'react'
 import './HomePage.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBell, faChartBar, faChartLine, faCircleInfo, faCog, faHome, faSearch, faUser, faWallet } from '@fortawesome/free-solid-svg-icons';
+import { Card, CardBody, CardText, CardTitle } from 'react-bootstrap';
+import Header from '../Common/Header';
 
 export default function HomePage() {
     return (
-        <Container fluid className="gridContainer p-0">
-            <Row>
-                <Col md="auto" className="menuBar">
-                    <img src='/ecash.png' style={{ marginTop: 20}} width={50} alt="Logo" />
-                    <ul className="list-unstyled">
-                        <li className="mb-5">
-                            <FontAwesomeIcon icon={faHome} size='lg' />
-                        </li>
-                        <li className="mb-5">
-                            <FontAwesomeIcon icon={faWallet} size='lg' />
-                        </li>
-                        <li className="mb-5">
-                            <FontAwesomeIcon icon={faUser} size='lg' />
-                        </li>
-                        <li className="mb-5">
-                            <FontAwesomeIcon icon={faBell} size='lg' />
-                        </li>
-                        <li className="mb-5">
-                            <FontAwesomeIcon icon={faCog} size='lg' />
-                        </li>
-                    </ul>
-                </Col>
-
-                <Col xs={20} md={11} className="dashboardContainer p-0">
-                    <div className="header d-flex justify-content-between align-items-left p-2.5">
-                        <div className="header-left">
-                            <Form className="searchGrid d-flex">
-                                <Form.Control
-                                    type="text"
-                                    placeholder="Search Here"
-                                    className="me-1"
-                                    style={{ background: "transparent", color: "#ccc", caretColor: "white" }}
-                                />
-                                <Button variant="outline-light" className="search-button">
-                                    <FontAwesomeIcon icon={faSearch} />
-                                </Button>
-                            </Form>
-                        </div>
-                        <div className="header-right">
-                            {/* Add any right header content here */}
-                        </div>
-                    </div>
-
-                    <div className="mainContent">
-                        <h2>Dashboard</h2>
-                    </div>
-                </Col>
-            </Row>
-        </Container>
-    );
+        <div>
+            <Header/>
+            <h1>Home Page</h1>
+        </div>
+    )
 }
 
-
-// import React from 'react'
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faBarChart, faBell, faCog, faHome, faPersonBooth, faPersonRifle, faSearch, faUser, faWallet } from '@fortawesome/free-solid-svg-icons';
-// import { faPerson } from '@fortawesome/free-solid-svg-icons/faPerson';
-// import { faAffiliatetheme } from '@fortawesome/free-brands-svg-icons';
-// import './HomePage.css';
-
-// export default function HomePage() {
-//     return (
-//         <div>
-
-//             <div className="gridContainer">
-//                 <div className="menuBar">
-//                     <img src='\ecash.png' style={{ marginTop: 20 }} width={50} />
-//                     <ul>
-//                         <li>
-//                             <FontAwesomeIcon icon={faHome} size='lg' border transform="shrink-6" />
-//                         </li>
-//                         <li>
-//                             <FontAwesomeIcon icon={faWallet} size='lg' border transform="shrink-6" />
-//                         </li>
-//                         <li>
-//                             <FontAwesomeIcon icon={faUser} size='lg' border transform="shrink-6" />
-//                         </li>
-//                         <li>
-//                             <FontAwesomeIcon icon={faBell} size='lg' border transform="shrink-6" />
-//                         </li>
-//                         <li>
-//                             <FontAwesomeIcon icon={faCog} size='lg' border transform="shrink-6" />
-//                         </li>
-//                     </ul>
+//         <div className='window'>
+//             <div className="division1 ">
+//                 <div className="logo">
+//                     <img src='ecash.png' />
 //                 </div>
 
-//                 <div className="dashboardContainer">
-//                     <div className="header">
-//                         <div className="header-left">
-//                             <form className="searchGrid">
-//                                 <div className='col-1 '>
-//                                     <input type="text" placeholder="Search Here" style={{ background: "transparent" }} />
-//                                 </div>
-//                                 <div className='col-2'>
-//                                     <FontAwesomeIcon icon={faSearch} />
-//                                 </div>
-//                             </form>
-//                         </div>
-//                         <div className="header-right">
+//                 <div className="home-icon">
+//                     <FontAwesomeIcon icon={faHome} />
+//                     <div className='p'>Home</div>
+//                 </div>
 
-//                         </div>
+//                 <div className="user-icon">
+//                     <FontAwesomeIcon icon={faUser} />
+//                     <div className='p'>Profile</div>
+//                 </div>
+
+
+//                 <div className="wallet-icon">
+//                     <FontAwesomeIcon icon={faWallet} />
+//                     <div className='p'>Wallet</div>
+//                 </div>
+
+//                 <div className="bell-icon">
+//                     <FontAwesomeIcon icon={faBell} />
+//                     <div className='p'>Notifications</div>
+//                 </div>
+
+//                 <div className="chart-icon">
+//                     <FontAwesomeIcon icon={faChartLine} />
+//                     <div className='p'>Analytics</div>
+//                 </div>
+
+//                 <div className="settings-icon">
+//                     <FontAwesomeIcon icon={faCog} />
+//                     <div className='p'>Settings</div>
+//                 </div>
+
+//             </div>
+//             <div className='division2'>
+//                 <header>
+//                     <div className='left-header'>
+//                         <form>
+//                             <input className='search-input' placeholder='Search here'></input>
+//                             <button className='search-button'><FontAwesomeIcon icon={faSearch}/></button>
+//                         </form>
 //                     </div>
 
-//                     <div className="mainContent">
-//                         <h2>Dashboard</h2>
+//                     <div className='right-header'>
+//                         <div className='notification-icon'><FontAwesomeIcon icon={faBell}/></div>
+//                         <div className='user-profile-icon'><FontAwesomeIcon icon={faUser}/></div>
 //                     </div>
+//                 </header>
+
+//                 <div className='tittle'>
+//                     <h4>Dashboard</h4>
+//                 </div>
+
+//                 <div className='body'>
+//                     <Card className='boxproperty'>
+//                         <CardBody>
+//                             <CardTitle>
+//                             Total Balance
+//                             </CardTitle>
+//                             <CardText>
+//                             $ 432568
+//                             </CardText>
+//                         </CardBody>
+//                     </Card>
+//                     <Card className='boxproperty'>
+//                         <CardBody>
+//                             <CardTitle>
+//                             Total Balance
+//                             </CardTitle>
+//                             <CardText>
+//                             $ 432568
+//                             </CardText>
+//                         </CardBody>
+//                     </Card>
+//                     <Card className='boxproperty'>
+//                         <CardBody>
+//                             <CardTitle>
+//                             Total Balance
+//                             </CardTitle>
+//                             <CardText>
+//                             $ 432568
+//                             </CardText>
+//                         </CardBody>
+//                     </Card>
+//                     <Card className='boxproperty'>
+//                         <CardBody>
+//                             <CardTitle>
+//                             Total Balance
+//                             </CardTitle>
+//                             <CardText>
+//                             $ 432568
+//                             </CardText>
+//                         </CardBody>
+//                     </Card>
+                    
 //                 </div>
 //             </div>
 //         </div>
