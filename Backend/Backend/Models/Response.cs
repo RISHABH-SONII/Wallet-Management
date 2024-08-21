d72d3db12caae9678b387cb9d44abc2931f1c458
RISHABH-SONII <rishabhsoni50414@gmail.com>
2024-08-21 21:51:08 +0530

    Adding response file to show the statusCode and statusMessage Of Api Working

---
+++ Backend/Backend/Models/Response.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Models
+{
+    public class Response
+    {
+        public int StatusCode { get; set; }
+        public string StatusMessage { get; set; }
+
+        public List<Users> listUsers { get; set; }
+
+        public Users user { get; set; }
+    }
+}
