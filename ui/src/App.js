8352f08b61503949916e7b3347ad012437e4ca06
RISHABH-SONII <rishabhsoni50414@gmail.com>
2024-08-21 22:04:55 +0530

    Adding App.js file to contain routes for different pages.

---
+++ ui/src/App.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import Layout from './Components/Layout/Layout'
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import HomePage from './Pages/HomePage';
+import UserProfile from './Pages/UserProfile';
+import Wallet from './Pages/Wallet';
+import Analytics from './Pages/Analytics';
+import Notifications from './Pages/Notifications';
+import Settings from './Pages/Settings';
+import PageNotFound from './Pages/Notifications'
+function App() {
+  return (
+    <div>
+    <BrowserRouter>
+    <Routes>
+        <Route path='/' element={ <HomePage/> }/>
+        <Route path='/userProfile' element={ <UserProfile/> }/>
+        <Route path='/wallet' element={ <Wallet/> }/>
+        <Route path='/analytics' element={ <Analytics/> }/>
+        <Route path='/notification' element={ <Notifications/> }/>
+        <Route path='/settings' element={ <Settings/> }/>
+        <Route path='*' element={ <PageNotFound/> }/>
+    </Routes>
+    </BrowserRouter>
+    </div>
+  )
+}
+
+export default App;
+
+{/* <Layout>
+        <h1>MyHeading</h1>
+        </Layout>           Layout component is the wrapper container */}
+
+
+        // let allroutes=createBrowserRouter(
+//   [
+//     {
+//       path:"/",
+//       element:<HomePage/>
+//     },
+//     {
+//       path:"/userProfile",
+//       element:<UserProfile/>
+//     },
+//     {
+//       path:"/wallet",
+//       element:<Wallet/>
+//     },
+//     {
+//       path:"/analytics",
+//       element:<Analytics/>
+//     },
+//     {
+//       path:"/notification",
+//       element:<Notifications/>
+//     },
+//     {
+//       path:"/settings",
+//       element:<Settings/>
+//     }
+//   ]
+// )
