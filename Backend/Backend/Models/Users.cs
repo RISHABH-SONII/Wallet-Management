d5a365b61356bac0e7f623506521ceb775d2d592
RISHABH-SONII <rishabhsoni50414@gmail.com>
2024-08-21 21:52:29 +0530

    Adding Users.cs file for containing variables to store the data.

---
+++ Backend/Backend/Models/Users.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Models
+{
+    public class Users
+    {
+        [Key]
+        public int UserId { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set;}
+        
+        
+    }
+}
