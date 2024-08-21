63188fdb93d224458083ad26045816fd694543cf
RISHABH-SONII <rishabhsoni50414@gmail.com>
2024-08-21 22:01:59 +0530

    Removing Unnecessary import files

--- ui/src/index.js
+++ ui/src/index.js
@@ -4,12 +4,12 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.css"
 import "bootstrap/dist/js/bootstrap.bundle"
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+
   <React.StrictMode>
     <App/>                {/* <RouterProvider router={allroutes}/> */}
   </React.StrictMode>
