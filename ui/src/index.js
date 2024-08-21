25ac3ddc7dd2954d7e366dd3e95ab1c7cad34e03
RISHABH-SONII <rishabhsoni50414@gmail.com>
2024-08-21 22:00:21 +0530

    Modify Index.js file by removing routes.

--- ui/src/index.js
+++ ui/src/index.js
@@ -4,52 +4,45 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.css"
 import "bootstrap/dist/js/bootstrap.bundle"
-import HomePage from './Pages/HomePage';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Notifications from './Pages/Notifications';
-import UserProfile from './Pages/UserProfile';
-import Wallet from './Pages/Wallet';
-import Settings from './Pages/Settings';
-import Analytics from './Pages/Analytics';
+import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-let allroutes=createBrowserRouter(
-  [
-    {
-      path:"/",
-      element:<HomePage/>
-    },
-    {
-      path:"/userProfile",
-      element:<UserProfile/>
-    },
-    {
-      path:"/wallet",
-      element:<Wallet/>
-    },
-    {
-      path:"/analytics",
-      element:<Analytics/>
-    },
-    {
-      path:"/notification",
-      element:<Notifications/>
-    },
-    {
-      path:"/settings",
-      element:<Settings/>
-    }
-  ]
-)
 root.render(
-
   <React.StrictMode>
-    <RouterProvider router={allroutes}/>
+    <App/>                {/* <RouterProvider router={allroutes}/> */}
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+// let allroutes=createBrowserRouter(
+//   [
+//     {
+//       path:"/",
+//       element:<HomePage/>
+//     },
+//     {
+//       path:"/userProfile",
+//       element:<UserProfile/>
+//     },
+//     {
+//       path:"/wallet",
+//       element:<Wallet/>
+//     },
+//     {
+//       path:"/analytics",
+//       element:<Analytics/>
+//     },
+//     {
+//       path:"/notification",
+//       element:<Notifications/>
+//     },
+//     {
+//       path:"/settings",
+//       element:<Settings/>
+//     }
+//   ]
+// )
