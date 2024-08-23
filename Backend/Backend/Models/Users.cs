8f0fb40b74a2f3ab2a9c263e961d7ca6986b175c
RISHABH-SONII <rishabhsoni50414@gmail.com>
2024-08-24 00:54:39 +0530

    Modify variables in Users.cs file

--- Backend/Backend/Models/Users.cs
+++ Backend/Backend/Models/Users.cs
@@ -4,13 +4,11 @@ namespace Backend.Models
 {
     public class Users
     {
-        [Key]
         public int UserId { get; set; }
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
-        public string Password { get; set; }
+        public string Password { get; set; } 
         public string Email { get; set; }
-        public string Role { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set;}
         
