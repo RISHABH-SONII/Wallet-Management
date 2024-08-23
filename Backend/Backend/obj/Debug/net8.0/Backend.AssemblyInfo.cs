c38cf34e3da534ea5bb111521f30cdb83b32166e
RISHABH-SONII <rishabhsoni50414@gmail.com>
2024-08-24 01:02:03 +0530

    making proper structure of the code

--- Backend/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+387f57c3230f4b549e0e09ddacd412cfc03de585")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d41f65570031a33b255feacfd9687ecdcda0a1df")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
