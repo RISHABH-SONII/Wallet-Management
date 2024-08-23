5a5bc6cb71596885378b4d5ec2714a5f490efdb1
RISHABH-SONII <rishabhsoni50414@gmail.com>
2024-08-24 00:56:43 +0530

    Adding more variables for wallet and transection under response file

--- Backend/Backend/Models/Response.cs
+++ Backend/Backend/Models/Response.cs
@@ -9,9 +9,12 @@ namespace Backend.Models
     {
         public int StatusCode { get; set; }
         public string StatusMessage { get; set; }
-
         public List<Users> listUsers { get; set; }
-
         public Users user { get; set; }
+        public List<Wallets> listWallets { get; set; }
+        public Wallets wallet { get; set; }
+        public List<Transections> listTransections { get; set; }
+        public Transections transections { get; set; }
+
     }
 }
