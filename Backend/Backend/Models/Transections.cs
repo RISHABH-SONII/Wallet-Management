212419a30af06a3a12f4d3dd621b74b66cf7561c
RISHABH-SONII <rishabhsoni50414@gmail.com>
2024-08-24 00:55:40 +0530

    Adding new model named Transections.cs for storing transection variables

---
+++ Backend/Backend/Models/Transections.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Backend.Models
+{
+    public class Transections
+    {
+        public int TransectionID { get; set; }
+        public int WalletID { get; set; }
+
+        //public int UserID { get; set; }
+        public Decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Category { get; set; }
+        public string Notes { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
