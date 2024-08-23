a075df195ea50c48077e2078ad0129339adba20d
RISHABH-SONII <rishabhsoni50414@gmail.com>
2024-08-24 00:27:08 +0530

    Modify layout file for adding the sidebar component

--- ui/src/Components/Layout/Layout.jsx
+++ ui/src/Components/Layout/Layout.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
+import { Box } from '@mui/material'
+import Sidebar from './Sidebar'
 
 const Layout = ({ children }) => {
   return (
     <>
-    <Header/>
-    <div>{children}</div>
-    <Footer/>
+    <Box sx={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
+      <Header/>
+        <Box sx={{ display: 'flex', flexGrow: 1}}>
+          <Sidebar />
+            <Box component="main" sx={{ flexGrow: 1, ml: '240px',mb:"2px",backgroundColor: '#14172B'}}> {/* Adjust ml and mt based on sidebar width and header height */}
+            <div>{children}</div>
+            </Box>
+        </Box>
+      <Footer/>
+    </Box>
     </>
   )
 }
