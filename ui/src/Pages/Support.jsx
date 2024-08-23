75b51b0693498dfc135f7e09500af6b9e5aaa134
RISHABH-SONII <rishabhsoni50414@gmail.com>
2024-08-24 00:42:04 +0530

    Modify Support.jsx file to use the layout component for showing header, sidebar, footer

---
+++ ui/src/Pages/Support.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../Components/Layout/Layout'
+
+export default function Support() {
+  return (
+    <Layout>
+    <h1>Support</h1>
+    </Layout>
+  )
+}
