d5d0209437738f1e7a08f8f9ca198906afcbb236
RISHABH-SONII <rishabhsoni50414@gmail.com>
2024-08-24 00:53:32 +0530

    Adding new model named Wallets.cs for storing wallet variables

---
+++ Backend/Backend/Models/Wallets.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Backend.Models
+{
+    public class Wallets
+    {
+        public int WalletID { get; set; }
+        public int UserID { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Type { get; set; }
+        public Decimal InitialBalance { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
