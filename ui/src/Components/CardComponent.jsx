b67b63b61e9f8db8f018937d4504994324c4d361
RISHABH-SONII <rishabhsoni50414@gmail.com>
2024-08-26 00:14:08 +0530

    Modify CardComponent.jsx

--- ui/src/Components/CardComponent.jsx
+++ ui/src/Components/CardComponent.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Card, CardContent, Typography } from '@mui/material';
+import './CardComponent.css';
 
 export default function CardComponent({ title, value, subtitle }) {
   return (
-    <Card sx={{ backgroundColor: '#1F2130', color: '#ffffff' }}>
+    <Card className='balanceCard' sx={{ backgroundColor: '#1F2130', color: '#ffffff' }}>
       <CardContent>
         <Typography variant="h6" gutterBottom>
           {title}
