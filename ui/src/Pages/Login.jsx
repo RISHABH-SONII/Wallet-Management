b4bb0d0985fb21b19106962c9ec70802b56d3f07
RISHABH-SONII <rishabhsoni50414@gmail.com>
2024-08-26 00:38:30 +0530

    Add Login.jsx To call the logincomponent

---
+++ ui/src/Pages/Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Layout from '../Components/Layout/Layout'
+import LoginComponent from '../Components/LoginComponent'
+
+export default function Login() {
+  return (
+         <LoginComponent/>
+  )
+}
