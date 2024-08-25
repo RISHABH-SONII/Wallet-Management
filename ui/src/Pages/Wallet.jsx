6129aa646d71d5050866ff89277880f2d6cafb27
RISHABH-SONII <rishabhsoni50414@gmail.com>
2024-08-26 00:39:49 +0530

    Modify Wallet.jsx

--- ui/src/Pages/Wallet.jsx
+++ ui/src/Pages/Wallet.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Layout from '../Components/Layout/Layout'
+import WalletComponent from '../Components/WalletComponent'
 
 export default function Wallet() {
   return (
     <Layout>
-      <h1>Wallet Page</h1>
+      <WalletComponent/>
     </Layout>
   )
 }
