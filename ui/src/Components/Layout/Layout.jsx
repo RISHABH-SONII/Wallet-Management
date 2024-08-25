5397e34c0e07319b7770e5f31063604ff887afe7
RISHABH-SONII <rishabhsoni50414@gmail.com>
2024-08-26 00:02:11 +0530

    Modify Layout.jsx

--- ui/src/Components/Layout/Layout.jsx
+++ ui/src/Components/Layout/Layout.jsx
@@ -1,23 +1,45 @@
-import React from 'react'
-import Header from './Header'
-import Footer from './Footer'
-import { Box } from '@mui/material'
-import Sidebar from './Sidebar'
+import React, { useEffect } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import { Box } from "@mui/material";
+import Sidebar from "./Sidebar";
+import { useNavigate } from "react-router-dom";
 
 const Layout = ({ children }) => {
+  const router = useNavigate();
+  useEffect(() => {
+    const userId = sessionStorage.getItem("authToken");
+    console.log(userId);
+    if (!userId) {
+      router("/login");
+    }
+  }, []);
+
   return (
     <>
-    <Box sx={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
-      <Header/>
-        <Box sx={{ display: 'flex', flexGrow: 1}}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          // height: "100vh",
+          // maxWidth: "100vw",
+        }}
+      >
+        <Header />
+        <Box sx={{ display: "flex", flexGrow: 1 }}>
           <Sidebar />
-            <Box component="main" sx={{ flexGrow: 1, ml: '240px',mb:"2px",backgroundColor: '#14172B'}}> {/* Adjust ml and mt based on sidebar width and header height */}
+          <Box
+            component="main"
+            sx={{ flexGrow: 1, ml: "215px", backgroundColor: "#14172B" }}
+          >
+            {" "}
+            {/* Adjust ml and mt based on sidebar width and header height */}
             <div>{children}</div>
-            </Box>
+          </Box>
         </Box>
-      <Footer/>
-    </Box>
+        <Footer />
+      </Box>
     </>
-  )
-}
+  );
+};
 export default Layout;
